026a6153f5b630f66b73596b523618077779de35
SiddharthSharechat <siddharth.singh@sharechat.co>
2024-07-30 13:22:40 +0530

    migrate file changes

--- Models/userModel.go
+++ Models/userModel.go
@@ -4,10 +4,10 @@ import "time"
 import "gorm.io/plugin/soft_delete"
 
 type User struct {
-	Id        string
-	Name      string
-	Email     string
-	Location  string
+	Id        string `gorm:"type:varchar(100);primaryKey"`
+	Name      string `gorm:"type:varchar(100)"`
+	Email     string `gorm:"type:varchar(100);unique_index"`
+	Location  string `gorm:"type:varchar(100)"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	IsDeleted soft_delete.DeletedAt `gorm:"softDelete:flag"`
